9b305c7bcc5a0c857171063436509479511434d0
M Krapp <mario_ist@gmx.de>
2014-10-07 14:13:27 +0200

    Update of example/Readme.md. Added plotting script (Python).

---
+++ example/plot_example.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import sys
+
+mpl.rcParams['figure.figsize'] = 6,12
+
+var_names = ['Surface Temperature (K)', 'Albedo', 'Snow Height (m)',
+             'SMB (mm/day)', 'Melt (mm/day)', 'Accumulation (mm/day)']
+
+data = np.loadtxt(sys.argv[1])
+vali = np.loadtxt(sys.argv[2])
+
+ndata = len(data[0,:])
+nsteps = len(data[:,0])
+
+f, ax = plt.subplots(6)
+x = np.arange(nsteps)
+lw = 2
+if nsteps>365: lw=1
+
+for y in range(ndata):
+    fac = 1.
+    if y >= 3: fac = 86.4e6
+    ax[y].plot(x,fac*data[:,y],'k-',lw=lw,alpha=0.75)
+    ax[y].plot(x,fac*vali[:,y],'r-',lw=lw,alpha=0.75)
+    ax[y].set_xlim(x[0],x[-1])
+    ax[y].grid()
+    ax[y].set_title(var_names[y])
+
+plt.tight_layout()
+plt.show()
+
