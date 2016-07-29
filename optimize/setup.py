9e478f932827eea3ed164d024df0cbf6b0e4b9bb
Mario Krapp <mariokrapp@gmail.com>
2016-07-29 17:49:57 +0200

    add python package install tools (optimization_algorithms)

---
+++ optimize/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='optimization_algorithms',
+      version='0.1',
+      description='Optimization algorithms',
+      url='https://github.com/mkrapp/semic/tree/master/optimize/optimization_algorithms',
+      author='Mario Krapp',
+      author_email='mariokrapp@gmail.com',
+      license='BSD',
+      packages=['optimization_algorithms'],
+      zip_safe=False)
